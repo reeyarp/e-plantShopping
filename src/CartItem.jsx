f2ff38983c44fbaf77084d1a8d76710b3bcf9537
reeyarp <reeyarpatel132@gmail.com>
2025-06-05 15:21:47 -0400

    final

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -31,14 +31,18 @@ const CartItem = ({ onContinueShopping }) => {
 
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+    const updatedItem = { ...item };
+    updatedItem.quantity++;
+    dispatch(updateQuantity(updatedItem));
   };
 
   const handleDecrement = (item) => {
-   if(item.quantity > 1){
-    dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
-   }else{
-    dispatch(removeItem(item.name));
+   const updatedItem = {... item };
+   if(updatedItem.quantity == 1){
+    dispatch(removeItem(updatedItem));
+   } else {
+    updatedItem.quantity--;
+    dispatch(updateQuantity(updatedItem));
    }
   };
 
